de55615d5701a19a4fa48156c8f1a60b564a2ac9
taroj khan <tarujkhan@yahoo.com>
2020-01-23 23:04:47 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,16 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+expect(speak_to_grandma('WHAT?')).to eq "NO, NOT SINCE 1938!"
+  end
+
+  it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
+    expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
+  end
+
+  it 'responds with HUH?! SPEAK UP, SONNY! unless you are shouting' do
+    expect(speak_to_grandma('Hi Nana, how are you?')).to eq 'HUH?! SPEAK UP, SONNY!'
+  end
+
+end
